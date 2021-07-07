33dad0297a6a0722cca4a858ca7d58f1e5e18078
loveyousomuch <loveyousomuch554@gmail.com>
2021-07-08 01:02:17 +0300

    some syntax

--- compile.sh
+++ compile.sh
@@ -5,13 +5,16 @@ realpath() {
 }
 
 CURR_DIR=$( realpath "$0" | sed 's|\(.*\)/.*|\1|' ) 
+
 if [ -z "$CURR_DIR" ] ; then
   # error; for some reason, the path is not accessible
   # to the script (e.g. permissions re-evaled after suid)
   exit 1  # fail
 fi
+
 _FILE=`basename ${1%.*}`
 FILE="${CURR_DIR}/out/${_FILE}.out"
 mkdir -p "${CURR_DIR}/out"
+
 iverilog -o $FILE $1
 vvp $FILE
