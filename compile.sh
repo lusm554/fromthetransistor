42c14101f2665562e18f84c5ace73d701e1a3f7e
Nikita <loveyousomuch554@gmail.com>
2021-07-08 00:44:31 +0300

    compile code in out dir and run it

--- compile.sh
+++ compile.sh
@@ -1,2 +1,13 @@
-iverilog -o a.out $1
-vvp a.out
+#!/bin/bash
+CURR_DIR=$( realpath "$0" | sed 's|\(.*\)/.*|\1|' ) 
+if [ -z "$CURR_DIR" ] ; then
+  # error; for some reason, the path is not accessible
+  # to the script (e.g. permissions re-evaled after suid)
+  exit 1  # fail
+fi
+_FILE=`basename ${1%.*}`
+FILE="${CURR_DIR}/out/${_FILE}.out"
+echo $FILE
+mkdir -p "${CURR_DIR}/out"
+iverilog -o $FILE $1
+vvp $FILE
