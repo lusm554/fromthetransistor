0318f9c8e19645910cecf8de23bf95952718e282
loveyousomuch <loveyousomuch554@gmail.com>
2021-07-08 03:19:03 +0300

    module structure

---
+++ learnverilog/module.v
@@ -0,0 +1,16 @@
+module example (a, b, c, y);
+  input a;
+  input b;
+  input c;  
+  output y;
+endmodule
+
+/* 
+* A module is the main building block in Verilog
+*
+* So, what we need to declare it?
+* Just:
+* - Name of the module
+* - Types of its connections (input, output)
+* - Name of its connection
+* */
