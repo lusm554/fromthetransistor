804f3aa8d3343628610d841eabbe32895445383a
loveyousomuch <loveyousomuch554@gmail.com>
2021-11-15 15:40:17 +0300

    not work

--- section-2/uart.py
+++ section-2/uart.py
@@ -4,4 +4,14 @@
 def bprint(b, zeroes=4):
     print(f'0b{b:0{zeroes}b}')
 
+bit = 0b1  
+# .bit_length()
+
+# baud - bits per second
+
+class Uart:
+    def __init__(self, date, baud=9600, bits=8, stop_bits=1):
+        self._baud = buad
+        self._bits = bits
+        self._stop_bits = stop_bits
 
