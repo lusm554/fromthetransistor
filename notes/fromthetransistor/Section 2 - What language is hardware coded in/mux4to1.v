631399e201954318cecaa40b8505e2859040d805
lusm <loveyousomuch554@gmail.com>
2025-06-23 15:20:58 +0300

    mux4to1

--- notes/fromthetransistor/Section 2 - What language is hardware coded in/mux4to1.v
+++ notes/fromthetransistor/Section 2 - What language is hardware coded in/mux4to1.v
@@ -13,11 +13,20 @@ endmodule
 
 module mux4to1_tb;
     reg [1:0] sel;
-    reg d0, d1, d2, d3, y;
+    reg d0, d1, d2, d3;
+    wire y;
 
     initial begin
-        $monitor("y=%b", y);
-        #5 sel = 2'b00;
+        $monitor("sel=%b, y=%b, d0=%b, d1=%b, d2=%b, d3=%b", sel, y, d0, d1, d2, d3);
+        d0 = 1;
+        d1 = 0;
+        d2 = 1;
+        d3 = 0;
+        #10 sel = 2'b00;
+        #10 sel = 2'b01;
+        #10 sel = 2'b10;
+        #10 sel = 2'b11;
+        $finish;
     end
 
     mux4to1 m0 (
@@ -25,7 +34,7 @@ module mux4to1_tb;
         .d0 (d0),
         .d1 (d1),
         .d2 (d2),
-        .d3 (d3)
+        .d3 (d3),
+        .y  (y)
     );
-
 endmodule
