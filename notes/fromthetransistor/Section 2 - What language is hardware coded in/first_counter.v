d7a3cdad143a5f48f1a000418d01733113ae8e93
lusm <loveyousomuch554@gmail.com>
2025-06-24 18:10:52 +0300

    add test bench

--- notes/fromthetransistor/Section 2 - What language is hardware coded in/first_counter.v
+++ notes/fromthetransistor/Section 2 - What language is hardware coded in/first_counter.v
@@ -1,3 +1,4 @@
+`timescale 1ns / 1ps
 module first_counter (
     input wire clock,
     input wire reset,
@@ -12,4 +13,36 @@ module first_counter (
             counter_out <= #1 counter_out + 1;
         end
     end
+endmodule
+
+module first_counter_tb;
+    reg clock, reset, enable;
+    wire [3:0] counter_out;
+
+    initial begin
+        $dumpfile("waves.vcd");
+        $dumpvars(0, first_counter_tb);
+        $display("time\tclk\treset\tenable\tcounter");
+        $monitor("%g\t%b\t%b\t%b\t%b", $time, clock, reset, enable, counter_out);
+        clock  = 1;
+        reset  = 0;
+        enable = 0;
+        #5 reset = 1;
+        #10 reset = 0;
+        #10 enable = 1;
+        #100 enable = 0;
+        #5 $finish;
+    end
+
+    always begin
+        #5 clock = ~clock;
+    end
+
+    first_counter f1 (
+        .clock(clock),
+        .reset(reset),
+        .enable(enable),
+        .counter_out(counter_out)
+    );
+
 endmodule
